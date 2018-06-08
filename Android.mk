df99a64fa6f4df0b81bacd514416a4fcb2b363a3
iamkarthiksb <34999043+iamkarthiksb@users.noreply.github.com>
2018-06-08 16:54:34 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter shamrock, $(TARGET_DEVICE)),)
+ifneq ($(filter CP3600I, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
